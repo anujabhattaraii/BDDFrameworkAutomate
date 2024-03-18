520e3d9b53cb77844e3d90334a3c2cc94e1c7822
Anuja Bhattarai <anujabhattarai@lftechnology.com>
2024-03-18 17:00:23 +1000

    first commit

---
+++ features/steps/login_method.py
@@ -0,0 +1,20 @@
+from behave import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@given('open browser')
+def open_browser(context):
+    context.driver = webdriver.Chrome()
+    context.driver.get("https://www.demo.guru99.com/test/newtours")
+     
+@when('providing valid username and password')
+def validUserNameAndPassword(context):
+    context.driver.find_element(By.NAME, "userName" ).send_keys("mercury")
+    context.driver.find_element(By.NAME, "password").send_keys("mercury")
+    context.driver.find_element(By.NAME, "submit").click()
+    
+@then('Verifying home')
+def verify_home(context):
+    assert "MERCURY TOURS" == context.driver.title
+    
